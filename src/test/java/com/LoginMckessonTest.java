62cdf05f1ba94598c8964bca19c64236f7f42d3c
rishibagekar <120618300+rishibagekar@users.noreply.github.com>
2024-09-19 12:16:23 +0530

    Update LoginMckessonTest.java

--- src/test/java/com/LoginMckessonTest.java
+++ src/test/java/com/LoginMckessonTest.java
@@ -27,7 +27,7 @@ public class LoginMckessonTest extends BaseClass {
 
 		ExtentTest test = extent.createTest("Login Process Verification");
 
-		LoginPageMKH LP = new LoginPageMKH();
+		LoginPageMKH LP = new LoginPageMKH(driver);
 		Logger.info("Add  credentials");
 
 		LP.username();
